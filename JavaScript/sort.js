7d99ce873006f29c1cd9960a5bdf31979a67bbe6
Jonathan Gregson <jdgregson@gmail.com>
2017-11-17 23:44:49 -0800

    JavaScript: combined Ascending and Descending into one if statement for each algorithm.

--- JavaScript/sort.js
+++ JavaScript/sort.js
@@ -3,12 +3,14 @@ var Sort = {
         ascending = typeof ascending!=='undefined'? ascending:true;
         for(var i=0; i<a.length; i++) {
             var smallSub = i;
-            for(var j=i+1; j<a.length; j++)
-                if(ascending && a[j] < a[smallSub]) {
-                    smallSub = j;
-                } else if(!ascending && a[j] > a[smallSub]) {
+            for(var j=i+1; j<a.length; j++) {
+                if(
+                    (ascending && a[j] < a[smallSub]) ||
+                    (!ascending && a[j] > a[smallSub])
+                ) {
                     smallSub = j;
                 }
+            }
             var temp = a[i];
             a[i] = a[smallSub];
             a[smallSub] = temp;
@@ -20,20 +22,14 @@ var Sort = {
         ascending = typeof ascending!=='undefined'? ascending:true;
         for(var i=1; i<a.length; i++) {
             var j = i;
-            if(ascending) {
-                while((j > 0) && (a[j] < a[j-1])) {
-                    var temp = a[j];
-                    a[j] = a[j-1];
-                    a[j-1] = temp;
-                    j = j-1;
-                }
-            } else {
-                while((j > 0) && (a[j] > a[j-1])) {
-                    var temp = a[j];
-                    a[j] = a[j-1];
-                    a[j-1] = temp;
-                    j = j-1;
-                }
+            while(
+                (ascending && (j > 0) && (a[j] < a[j-1])) ||
+                (!ascending && (j > 0) && (a[j] > a[j-1]))
+            ) {
+                var temp = a[j];
+                a[j] = a[j-1];
+                a[j-1] = temp;
+                j = j-1;
             }
         }
         return a;
@@ -43,18 +39,10 @@ var Sort = {
         ascending = typeof ascending!=='undefined'? ascending:true;
         for(var i=0; i<a.length-1; i++) {
             for(var j=0; j<a.length-1; j++) {
-                if(ascending) {
-                    if(a[j] > a[j+1]) {
-                        var temp = a[j];
-                        a[j] = a[j+1];
-                        a[j+1] = temp;
-                    }
-                } else {
-                    if(a[j] < a[j+1]) {
-                        var temp = a[j];
-                        a[j] = a[j+1];
-                        a[j+1] = temp;
-                    }
+                if((ascending && a[j]>a[j+1])||(!ascending && a[j]<a[j+1])) {
+                    var temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
                 }
             }
         }
